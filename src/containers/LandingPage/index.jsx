a5c405622664d84c88e9cc370d7b5afca79c8687
Kenneth Aduan <kenneth.aduan10@gmail.com>
2023-01-18 19:58:15 +0800

    Landing Complete

--- src/containers/LandingPage/index.jsx
+++ src/containers/LandingPage/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import Header from "../../components/Header/HeaderLanding/Header";
+import Hero from "./Hero.jsx";
+import Feature from "./Feature";
+import Service from "./Service";
+import Team from "./Team";
+import Footer from "../../components/Footer/FooterLanding/Footer";
+import WOW from "wow.js";
+import "../../styles/animate.css";
+import "../../styles/lineIcons.css";
+import "../../styles/Style.css";
+import "../../styles/tailwind.css";
 const index = () => {
-  return <div>
-    <Header/>
-  </div>;
+  new WOW().init();
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Feature />
+      <Service />
+      <Team />
+      <Footer />
+    </div>
+  );
 };
 
 export default index;
